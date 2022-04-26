17bff270d6d14264d52208c9054b1171949b7b86
Sowmiya <101380469+sowmiya1807@users.noreply.github.com>
2022-04-26 15:14:02 +0530

    Create main.c

---
+++ Implementation/main.c
@@ -0,0 +1,33 @@
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
+
+int main(void)
+{
+    uint16_t temp;
+    
+    while(1)
+    {
+        if(activity1_LED()==1) //Check if both the switches are pressed
+        {
+           
+            TurnLED_ON();//Turn the LED ON
+
+            temp=activity2_GetADC(); //Get the ADC value
+
+            activity3_PWM(temp); //PWM output based on the temperature
+
+		    activity4_USARTWrite(temp); //To Serial monitor to print Temperature
+            
+
+        }
+        else  //in all other cases
+        {
+            TurnLED_OFF();//Turn LED OFF
+		    _delay_ms(200);
+        }
+
+    }
+    return 0;
+}
